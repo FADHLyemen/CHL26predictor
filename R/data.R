ce2c0621379458fcdc58590904fdeddab1f9d451
Fong Chun Chan <fongchunchan@gmail.com>
2015-07-23 14:46:03 -0700

    Documentating Model Coefficients Data

---
+++ R/data.R
@@ -0,0 +1,6 @@
+#' CHL26 Model Coefficients
+#'
+#' Predictor feature coefficients as published in paper.
+#'
+#' @format A data.frame with 2 variables: \code{geneName}, \code{coef}
+"CHL26.model.coef.df"
