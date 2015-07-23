f1bc9dd4b9f6de145a1c80c1ba55d4373b7ebcc9
Fong Chun Chan <fongchunchan@gmail.com>
2015-07-23 15:00:41 -0700

    Adding Housekeepers to RData

--- data-raw/CHL26.model.coef.df.R
+++ data-raw/CHL26.model.coef.df.R
@@ -1,3 +1,14 @@
+library("dplyr")
+
 CHL26.model.coef.df <- read.table("data-raw/model-coef.tsv", header = TRUE,
                                   stringsAsFactor = FALSE)
+
+hk.df <- data.frame(geneName = c("ACTB", "CLTC", "RPLP0"),
+                    coef = NA,
+                    feature = "housekeeper")
+
+CHL26.model.coef.df <- CHL26.model.coef.df %>%
+  mutate(feature = "predictor") %>%
+  rbind(hk.df)
+
 devtools::use_data(CHL26.model.coef.df, overwrite = TRUE)
