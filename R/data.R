693c51ecc39de543890f42f1bce5700adc567c8f
fongchun <fongchunchan@gmail.com>
2015-07-24 18:52:39 +0000

    Adding Documentation for Test Expression Matrix

--- R/data.R
+++ R/data.R
@@ -4,3 +4,10 @@
 #'
 #' @format A data.frame with 2 variables: \code{geneName}, \code{coef}
 "CHL26.model.coef.df"
+
+#' CHL26 Test Expression Matrix
+#'
+#' A synthetically generated expression matrix to be used to test the package
+#'
+#' @format A 26 by 10 matrix
+"CHL26.test.exprs.mat"
